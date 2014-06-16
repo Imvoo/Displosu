b70ee945b0040833721d0883b65cc400483379e6
Kevin <Imvooz@gmail.com>
2014-06-16 23:02:21 +1000

    Added JSON demarshalling/deserialization for the Song entries. Added a function that makes error-checking easier.

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -17,14 +18,31 @@ var (
 	userID           string = "Imvoo"
 )
 
+type Song struct {
+	Beatmap_ID   string
+	Score        string
+	MaxCombo     string
+	Count50      string
+	Count100     string
+	Count300     string
+	CountMiss    string
+	CountKatu    string
+	CountGeki    string
+	Perfect      string
+	Enabled_Mods string
+	User_ID      string
+	Date         string
+	Rank         string
+}
+
 func mainPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello there, you are currently at %s. The URL is: %s. Response: %s",
-		r.URL.Path[1:], buildRecentURL(userID, 0), getRecentPlays(buildRecentURL(userID, 0)))
+	fmt.Fprintf(w, "Hello there, you are currently at %s. The URL is: %s.",
+		r.URL.Path[1:], buildRecentURL(userID, 0))
 }
 
 func main() {
 	setAPIKey()
-	fmt.Printf("Server started on Port %s.", PORT)
+	fmt.Printf("Server started on Port %s.\n", PORT)
 	http.HandleFunc("/", mainPage)
 	http.ListenAndServe(LISTEN_PORT, nil)
 }
@@ -33,9 +51,7 @@ func setAPIKey() {
 	var err error
 	API_KEY, err = ioutil.ReadFile("./APIKEY.txt")
 
-	if err != nil {
-		log.Fatal("ERROR: ", err)
-	}
+	checkError(err, "API Key")
 
 	// Trims spaces and trailing newlines from the API key so that the URL
 	// to retrieve songs can be built properly.
@@ -49,20 +65,32 @@ func buildRecentURL(USER_ID string, GAME_TYPE int) string {
 	return API_URL + API_RECENT_PLAYS + "?k=" + string(API_KEY) + "&u=" + USER_ID
 }
 
+func checkError(err error, msg string) {
+	if err != nil {
+		log.Fatal("ERROR (", msg, "): ", err)
+	}
+}
+
 func getRecentPlays(url string) string {
 	res, err := http.Get(url)
 	defer res.Body.Close()
 
-	if err != nil {
-		log.Fatal("ERROR: ", err)
-	}
+	checkError(err, "Get HTTP")
 
 	html, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
 
-	if err != nil {
-		log.Fatal("ERROR: ", err)
+	checkError(err, "Read HTML")
+
+	var songs []Song
+	err = json.Unmarshal(html, &songs)
+
+	checkError(err, "Unmarshal JSON")
+
+	// Prints out each song's entry, used for debugging purposes.
+	for i := 0; i < len(songs); i++ {
+		fmt.Printf("%s: ID=%s> Score=%s, %s\n",
+			string(songs[i].Date), songs[i].Beatmap_ID, songs[i].Score, songs[i].Rank)
 	}
 
-	return string(html)
+	return ""
 }
