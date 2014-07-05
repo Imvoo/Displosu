73e854adf8e3305e1b6ee13023d88bef8fc2b0e0
Kevin <Imvooz@gmail.com>
2014-07-05 15:45:48 +1000

    Added user-id handling.

--- server.go
+++ server.go
@@ -11,22 +11,25 @@ var (
 	PORT        string = "8080"
 	LISTEN_PORT string = ":" + PORT
 	DATABASE    GOsu.Database
+	USER_ID     string
 )
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello there, you are currently at %s. The URL is: %s.",
-		r.URL.Path[1:], DATABASE.BuildRecentURL(GOsu.USER_ID, 0))
+		r.URL.Path[1:], DATABASE.BuildRecentURL(USER_ID, 0))
 }
 
 func main() {
 	DATABASE.SetAPIKey()
-	GOsu.SetUserID("Imvoo")
+	USER_ID = "Imvoo"
 	fmt.Printf("Server started on Port %s.\n", PORT)
-	_, err := GOsu.GetRecentPlays(DATABASE.BuildRecentURL(GOsu.USER_ID, 0))
+	songs, err := DATABASE.GetRecentPlays(USER_ID, 0)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println(songs)
 
 	http.HandleFunc("/", mainPage)
 	http.ListenAndServe(LISTEN_PORT, nil)
