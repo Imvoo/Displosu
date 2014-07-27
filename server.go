820befdb23e3e74473bb9cd92d9dfe2167f36882
Kevin Imvoo <Imvooz@gmail.com>
2014-07-27 14:28:52 +1000

    Added environment variable tracking for database.
    The server now also checks the environment variables for a database
    username and database password, as well as a port for future use with
    Heroku.

--- server.go
+++ server.go
@@ -14,12 +14,14 @@ import (
 )
 
 var (
-	PORT           string = "8080"
-	LISTEN_PORT    string = ":" + PORT
+	PORT           string = os.Getenv("PORT")
+	LISTEN_PORT    string
 	DATABASE       GOsu.Database
 	USER_ID        string
 	session        *mgo.Session
 	collectionName string = "Imvoo"
+	dbUser         string
+	dbPass         string
 )
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +29,12 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// For use with local server and not Heroku.
+	if PORT == "" {
+		PORT = "8080"
+	}
+	LISTEN_PORT = ":" + PORT
+
 	DATABASE.SetAPIKey()
 	USER_ID = "Imvoo"
 
@@ -36,17 +44,33 @@ func main() {
 		log.Fatal(err)
 	}
 
-	userName, err := ioutil.ReadFile(dir + "\\dbUser.txt")
+	tempUserName, err := ioutil.ReadFile(dir + "\\dbUser.txt")
 	if err != nil {
-		log.Fatal(err)
+		dbUser = os.Getenv("dbUser")
+
+		if len(dbUser) == 0 {
+			log.Fatal("Unable to find a username for the MongoDB database in local file (dbUser.txt) or in the environment variables under dbUser.")
+		} else {
+			err = nil
+		}
+	} else {
+		dbUser = string(tempUserName)
 	}
 
-	password, err := ioutil.ReadFile(dir + "\\dbPass.txt")
+	tempPassword, err := ioutil.ReadFile(dir + "\\dbPass.txt")
 	if err != nil {
-		log.Fatal(err)
+		dbPass = os.Getenv("dbPass")
+
+		if len(dbPass) == 0 {
+			log.Fatal("Unable to find a password for the MongoDB database in local file (dbPass.txt) or in the environment variables under dbPass.")
+		} else {
+			err = nil
+		}
+	} else {
+		dbPass = string(tempPassword)
 	}
 
-	mongoDB := "mongodb://" + string(userName) + ":" + string(password) + "@ds053439.mongolab.com:53439/displosu"
+	mongoDB := "mongodb://" + string(dbUser) + ":" + string(dbPass) + "@ds053439.mongolab.com:53439/displosu"
 
 	session, err = mgo.Dial(mongoDB)
 	if err != nil {
